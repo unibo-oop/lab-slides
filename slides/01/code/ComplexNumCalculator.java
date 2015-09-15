c709b4e146b8dcf4229ee1c4a2791c397693499a
Matteo Casadei <mcasadei@imolinfo.it>
2015-09-15 21:32:27 +0200

    Modify git ignore and also first commit of lab 1.

---
+++ slides/01/code/ComplexNumCalculator.java
@@ -0,0 +1,14 @@
+public class ComplexNumCalculator {
+
+  public static void main (String args[]){
+    /*
+     * 1) Testare la classe con le seguenti operazioni tra numeri complessi:
+     * - add(1+2i, 2+3i) = 3+5i
+     * - sub(4+5i, 6+7i) = -2-2i
+     * - mul(8+2i, 3-i) = 26-2i 
+     * - ... altre a piacere
+     * 2) Verificare il corretto valore dei campi nOpDone, lastRes
+     * 3) Fare altre prove con operazioni a piacere
+     * */
+  }
+}
---
+++ slides/01/code/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+  
+  //... Aggiungere qui la definizione dei campi
+  
+
+  void build(/*Aggiungere i parametri di input */){
+    //... Inizializzazione dei campi della classe
+  }
+  
+  void printStudentInfo(){
+    //... Aggiungere i comandi per la stampa delle informazioni sullo studente
+  }
+
+  public static void main(String args[]){
+    Student student = new Student();
+    student.build(); // aggiornare l'invocazione del metodo build con l'aggiunta dei parametri
+    student.printStudentInfo();
+    /*
+     * Creare dei nuovi oggetti relativi agli studenti:
+     * 
+     * - Nome: Luigi Cognome: Gentile id: 1015, matriculationYear: 2012
+     * - Nome: Simone Cognome: Bianchi id: 1016, matriculationYear: 2010
+     * - Nome: Andrea Cognome: Bracci id: 1017, matriculationYear: 2012
+     * 
+     * Stampare a video le informazioni relative a ciascuno studente.
+     */ 
+  } 
+}
