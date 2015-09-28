afd6601883ebda9bea5dcd2c5ef4523d814ca152
Matteo Casadei <mcasadei@imolinfo.it>
2015-09-28 21:39:28 +0200

    Modify slides and add solutions lab 1.

---
+++ workspace/oop1516-lab01_sol/Train.java
@@ -0,0 +1,42 @@
+public class Train {
+
+  int nTotSeats;
+  int nFCSeats;
+  int nSCSeats;
+
+  int nFCReservedSeats;
+  int nSCReservedSeats;
+  
+  void build(int nTotSeats, int nFCSeats, int nSCSeats) {
+    this.nTotSeats = nTotSeats;
+    this.nFCSeats = nFCSeats;
+    this.nSCSeats = nSCSeats;
+    this.nFCReservedSeats = 0;
+    this.nFCReservedSeats = 0;
+  }
+  
+  void reserveFCSeats(int nSeats){
+    nFCReservedSeats += nSeats;
+  }
+  
+  void reserveSCSeats(int nSeats){
+    nSCReservedSeats += nSeats;    
+  }
+
+  double getTotOccupancyRatio(){
+    return   ((double)(nFCReservedSeats + nSCReservedSeats) / nTotSeats) * 100;  
+  }   
+
+  double getFCOccupancyRatio(){
+    return (((double) nFCReservedSeats) / nFCSeats)  * 100;
+  }
+  
+  double getSCOccupancyRatio(){
+    return (((double) nSCReservedSeats) / nSCSeats)  * 100;
+  }
+  
+  void deleteAllReservations(){
+    nFCReservedSeats = 0;
+    nSCReservedSeats = 0;
+  }
+}
