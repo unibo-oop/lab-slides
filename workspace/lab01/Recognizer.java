71ae24b83d0b54b95fd159d06284a45bd8a56ee0
Matteo Casadei <mcasadei@imolinfo.it>
2015-10-02 22:42:02 +0200

    include .class in files to be ignored. Add nextExpectedInt among the methods to be implemented on Recognizer.

--- workspace/lab01/Recognizer.java
+++ workspace/lab01/Recognizer.java
@@ -1,12 +1,13 @@
 public class Recognizer {
 
-  public static void main(String args[]){
+    public static void main(String args[]){
     /*
      * 1) Creare un oggetto della classe Recognizer
      * 2) Realizzare i metodi check1, check2, check3 e check4, che accettano in ingresso un intero, ritornando true soltanto se
      *    il valore passato e uguale rispettivamente a 1, 2, 3, 4
      * 3) Invocare i metodi check1, check2, ... passando in input una sequenza corretta e stampando in output
      *    il valore di ritorno relativo all'invocazione di ciascun metodo
+     *    - far precedere l'invocazione di ogni metodo check dall'invocazione di nextExpectedInt
      * 4) Invocare il metodo reset al termine del test su ogni sequenza
      * 5) Testare il riconoscimento per le seguenti sequenze con procedimento analogo a quello appena descritto 
      *    - 1,2,3,4,1,2,3,4 
@@ -15,13 +16,18 @@ public class Recognizer {
      *    - ...
      * 
      * ESEMPIO DI INVOCAZIONE: riconoscimento sequenza 1,2,3,4,5
-     *  
+     * 
+     * System.out.println("Next expected int is: " + recognizer.nextExpectedInt());
      * System.out.println("Now the result of the check is: " + recognizer.check1(1));
+     * System.out.println("Next expected int is: " + recognizer.nextExpectedInt());
      * System.out.println("Now the result of the check is: " + recognizer.check2(2));
+     * System.out.println("Next expected int is: " + recognizer.nextExpectedInt());
      * System.out.println("Now the result of the check is: " + recognizer.check3(3));
+     * System.out.println("Next expected int is: " + recognizer.nextExpectedInt());
      * System.out.println("Now the result of the check is: " + recognizer.check4(4));
+     * System.out.println("Next expected int is: " + recognizer.nextExpectedInt());
      * System.out.println("Now the result of the check is: " + recognizer.check1(5));
      * 
      */
-  }
+    }
 }
