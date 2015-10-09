1f175f8ae12130879abb858da937f035f34fd3d4
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-09 18:28:45 +0200

    Fix student solution

--- workspace/lab02_sol/src/oop/lab02/constructors/Student.java
+++ workspace/lab02_sol/src/oop/lab02/constructors/Student.java
@@ -1,23 +1,23 @@
 package oop.lab02.constructors;
 
 public class Student {
-  
-  String name;
-  String surname;
-  int id;
-  int matriculationYear;
-  
-  Student (int id, String name, String surname, int matriculationYear){
-    this.id = id;
-    this.name = name;
-    this.surname = surname;
-    this.matriculationYear = matriculationYear;
-  }
-  
-  void printStudentInfo(){
-    System.out.println("Student id: " + this.id);
-    System.out.println("Student name: " + this.name);
-    System.out.println("Student surname: " + this.surname);
-    System.out.println("Student matriculationYear: " + this.matriculationYear + "\n");
-  }
+
+	String name;
+	String surname;
+	int id;
+	int matriculationYear;
+
+	Student(final int id, final String name, final String surname, final int matriculationYear) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.matriculationYear = matriculationYear;
+	}
+
+	void printStudentInfo() {
+		System.out.println("Student id: " + this.id);
+		System.out.println("Student name: " + this.name);
+		System.out.println("Student surname: " + this.surname);
+		System.out.println("Student matriculationYear: " + this.matriculationYear + "\n");
+	}
 }
