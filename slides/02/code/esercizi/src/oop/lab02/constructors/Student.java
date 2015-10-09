e6baa02ce54fa6650fbf039f0766b8cde7d653d6
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-09 16:48:33 +0200

    Add modified version of last year's

---
+++ slides/02/code/esercizi/src/oop/lab02/constructors/Student.java
@@ -0,0 +1,42 @@
+package oop.lab02.constructors;
+
+public class Student {
+  
+  String name;
+  String surname;
+  int id;
+  int matriculationYear;
+  
+  void build(int id, String name, String surname, int matriculationYear){
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.matriculationYear = matriculationYear;
+  }
+  
+  void printStudentInfo(){
+    System.out.println("Student id: " + this.id);
+    System.out.println("Student name: " + this.name);
+    System.out.println("Student surname: " + this.surname);
+    System.out.println("Student matriculationYear: " + this.matriculationYear + "\n");
+  }
+
+  public static void main(String args[]){
+    Student student = new Student();
+    student.build(1014, "Mario", "Rossi", 2013);
+    student.printStudentInfo();
+    
+    student = new Student();
+    student.build(1015, "Luigi", "Gentile", 2012);
+    student.printStudentInfo();
+    
+    student = new Student();
+    student.build(1016, "Simone", "Bianchi", 2010);
+    student.printStudentInfo();
+
+    student = new Student();
+    student.build(1017, "Andrea", "Bracci", 2012);
+    student.printStudentInfo();
+
+  }
+}
