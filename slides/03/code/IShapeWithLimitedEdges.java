2849763d79918d155842c27e47c60adfa7ca45fa
Matteo Casadei <mcasadei@imolinfo.it>
2015-10-14 16:16:13 +0200

    Revert "Add initial revision of slides for 03 lab."
    This reverts commit 267c825381614907f57b44e2d1324e7a61dfb085.

--- slides/03/code/IShapeWithLimitedEdges.java
+++
@@ -1,5 +0,0 @@
-package oop1415.lab03.interfaces;
-
-public interface IShapeWithLimitedEdges extends IShape{
-  int getNumEdges();
-}
--- slides/03/code/IUser.java
+++
@@ -1,7 +0,0 @@
-package oop1415.lab03.acme;
-
-public interface IUser {
-  String getUsername();
-  String getPassword();
-  String getDescription();
-}
