267c825381614907f57b44e2d1324e7a61dfb085
Matteo Casadei <mcasadei@imolinfo.it>
2015-10-14 16:14:09 +0200

    Add initial revision of slides for 03 lab.

---
+++ slides/03/code/IShapeWithLimitedEdges.java
@@ -0,0 +1,5 @@
+package oop1415.lab03.interfaces;
+
+public interface IShapeWithLimitedEdges extends IShape{
+  int getNumEdges();
+}
---
+++ slides/03/code/IUser.java
@@ -0,0 +1,7 @@
+package oop1415.lab03.acme;
+
+public interface IUser {
+  String getUsername();
+  String getPassword();
+  String getDescription();
+}
