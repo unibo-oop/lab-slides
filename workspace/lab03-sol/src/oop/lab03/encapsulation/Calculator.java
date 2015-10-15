8dd37f5885f3970325bf1a84ce16001a875c7a80
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-15 15:54:02 +0200

    Improve formatting.

--- workspace/lab03-sol/src/oop/lab03/encapsulation/Calculator.java
+++ workspace/lab03-sol/src/oop/lab03/encapsulation/Calculator.java
@@ -17,7 +17,7 @@ public class Calculator {
 	public double getLastRes() {
 		return this.lastRes;
 	}
-	
+
 	private double updateStatus(final double val) {
 		this.lastRes = val;
 		this.nOpDone++;
@@ -25,7 +25,7 @@ public class Calculator {
 	}
 
 	public double add(final double n1, final double n2) {
-		return updateStatus(n1+ n2);
+		return updateStatus(n1 + n2);
 	}
 
 	public double sub(final double n1, final double n2) {
@@ -39,24 +39,20 @@ public class Calculator {
 	public double div(final double n1, final double n2) {
 		return updateStatus(n1 / n2);
 	}
-	
+
 	private static void printCalculatorStatus(final Calculator calc) {
 		System.out.println("nOpDone=" + calc.nOpDone);
 		System.out.println("lastRes=" + calc.lastRes + "\n");
 	}
 
-	public static void main(final String args[]) {
+	public static void main(final String[] args) {
 		final Calculator calc = new Calculator();
-
 		System.out.println("1+2=" + calc.add(1, 2));
 		printCalculatorStatus(calc);
-
 		System.out.println("-1-(+2)=" + calc.sub(-1, 2));
 		printCalculatorStatus(calc);
-
-		System.out.println("6*3=" + calc.mul(6, 3));
+		System.out.println("8*3=" + calc.mul(8, 3));
 		printCalculatorStatus(calc);
-
 		System.out.println("8/4=" + calc.div(8, 4));
 		printCalculatorStatus(calc);
 	}
