f678511fa57068bf4cd7d72e145b33ea96a47dfb
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-15 15:47:52 +0200

    Style fixes

--- workspace/lab03-sol/src/oop/lab03/interfaces/TestIBankAccount.java
+++ workspace/lab03-sol/src/oop/lab03/interfaces/TestIBankAccount.java
@@ -5,6 +5,10 @@ import oop.lab03.encapsulation.BankAccount;
 
 public class TestIBankAccount {
 	
+	private static final int INTIAL_AMOUNT = 10000;
+	private static final int WITHDRAW_AMOUNT = 15000;
+	
+	
 	private TestIBankAccount() {
 	}
 
@@ -28,17 +32,17 @@ public class TestIBankAccount {
 		 */
 		acc2 = new StrictBankAccount(usr2.getUserID(), 0);
 		// 7) Depositare 10000$ in entrambi i conti
-		acc1.deposit(usr1.getUserID(), 10000);
-		acc2.deposit(usr2.getUserID(), 10000);
+		acc1.deposit(usr1.getUserID(), INTIAL_AMOUNT);
+		acc2.deposit(usr2.getUserID(), INTIAL_AMOUNT);
 		// 8) Prelevare 15000$ in entrambi i conti
-		acc1.withdraw(usr1.getUserID(), 15000);
-		acc2.withdraw(usr2.getUserID(), 15000);
+		acc1.withdraw(usr1.getUserID(), WITHDRAW_AMOUNT);
+		acc2.withdraw(usr2.getUserID(), WITHDRAW_AMOUNT);
 		// 9) Stampare in stdout l'ammontare corrente
 		System.out.println("Mario Rossi account balance is " + acc1.getBalance());
 		System.out.println("Luigi Bianchi account balance is " + acc2.getBalance());
 		// 11) Depositare nuovamente 10000$ in entrambi i conti
-		acc1.deposit(usr1.getUserID(), 10000);
-		acc2.deposit(usr2.getUserID(), 10000);
+		acc1.deposit(usr1.getUserID(), INTIAL_AMOUNT);
+		acc2.deposit(usr2.getUserID(), INTIAL_AMOUNT);
 		// 11) Invocare il metodo computeManagementFees su entrambi i conti
 		acc1.computeManagementFees(usr1.getUserID());
 		acc2.computeManagementFees(usr2.getUserID());
