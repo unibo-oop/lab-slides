39524ffa4316f58c623900fa55dfce5344d563b2
Matteo Casadei <mcasadei@imolinfo.it>
2015-10-21 19:39:11 +0200

    Add first revised version (checkstyle, pmd and the like to be added) of lab 04 code.

---
+++ workspace/lab04/src/it/unibo/oop/lab04/bank/TestIBankAccount.java
@@ -0,0 +1,37 @@
+package it.unibo.oop.lab04.bank;
+
+public final class TestIBankAccount {
+
+    private TestIBankAccount() {
+    }
+
+    public static void main(final String[] args) {
+
+        final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
+        final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
+
+        final BankAccount acc1 = new SimpleBankAccount(usr1.getUserID(), 0);
+
+        /*
+         * Change the implementation from StrictBankAccount to
+         * ExtendedStrictBankAccount
+         */
+        final BankAccount acc2 = new StrictBankAccount(usr2.getUserID(), 0);
+
+        acc1.deposit(usr1.getUserID(), 10000);
+        acc2.deposit(usr2.getUserID(), 10000);
+        acc1.withdraw(usr1.getUserID(), 15000);
+        acc2.withdraw(usr2.getUserID(), 15000);
+
+        System.out.println("Mario Rossi account balance is " + acc1.getBalance());
+        System.out.println("Luigi Bianchi account balance is " + acc2.getBalance());
+
+        acc1.deposit(usr1.getUserID(), 10000);
+        acc2.deposit(usr2.getUserID(), 10000);
+        acc1.computeManagementFees(usr1.getUserID());
+        acc2.computeManagementFees(usr2.getUserID());
+
+        System.out.println("Mario Rossi account balance is " + acc1.getBalance());
+        System.out.println("Luigi Bianchi account balance is " + acc2.getBalance());
+    }
+}
