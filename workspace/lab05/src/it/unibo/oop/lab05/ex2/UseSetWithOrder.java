897559ed2ce1c31eb4e3d59cd7d46c248d06bf81
Matteo Casadei <mcasadei@imolinfo.it>
2015-10-29 22:11:59 +0100

    Add first version (just reformatting has been done) of lab05's code.

---
+++ workspace/lab05/src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -0,0 +1,32 @@
+package it.unibo.oop.lab05.ex2;
+
+/**
+ * 
+ * @author Mirko Viroli
+ * @author Danilo Pianini
+ *
+ */
+public final class UseSetWithOrder {
+
+    private UseSetWithOrder() {
+    }
+
+    /**
+     * @param s
+     *            ignored
+     */
+    public static void main(final String[] s) {
+
+        /*
+         * Write a program which:
+         * 
+         * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
+         * a new Comparator in a separate class.
+         * 
+         * 2) Inserts in the set a hundred strings containing randomly generated
+         * numbers (use Math.random())
+         * 
+         * 3) Prints the set, which must be ordered
+         */
+    }
+}
---
+++ workspace/lab05/src/it/unibo/oop/lab05/packages/pkg2/MoreDummy.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package it.unibo.oop.lab05.packages.pkg2;
+
+import it.unibo.oop.lab05.packages.pkg1.DummyClass;
+
+/**
+ * Dummy class, just to try by-hand compilation.
+ * 
+ * @author Danilo Pianini
+ *
+ */
+public class MoreDummy extends DummyClass {
+
+    @Override
+    public String toString() {
+        return "More";
+    }
+
+}
