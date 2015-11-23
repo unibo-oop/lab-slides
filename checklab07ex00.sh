0b3da208bb90e9d4a56a7282bbb905267f53aa3c
Danilo Pianini <danilo.pianini@unibo.it>
2015-11-23 14:48:08 +0100

    Add control to lab07x00 correction algorithm

--- checklab07ex00.sh
+++ checklab07ex00.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd "${1}"
 trash-put bin -r
 trash-put .hgignore
 trash-put *
-hg update --clean
+hg update tip --clean
 echo "Directory content:"
 ls -ahl
 echo
@@ -16,5 +17,6 @@ hg update -r4 --clean
 echo "Working directory at r4:"
 ls -a
 echo
-hg update --clean
+hg update tip --clean
 hg log -G
+hg heads
