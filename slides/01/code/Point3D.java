16b24440f48fc4117cd1b3ef05c83f549a16f8a8
Danilo Pianini <danilo.pianini@unibo.it>
2017-09-29 11:39:53 +0200

    Fix spacing

--- slides/01/code/Point3D.java
+++ slides/01/code/Point3D.java
@@ -2,21 +2,24 @@ class Point3D{   // dichiarazione classe
   double x;      // 3 campi
   double y;
   double z;
-  void build(double a,double b,double c){ // build
+  
+  void build(double a,double b,double c){
     this.x=a; this.y=b; this.z=c;
   }
-  double getModulo2(){                    // getModulo2
-    return this.x*this.x+this.y*this.y+this.z*this.z;
+  
+  double getModulo2(){
+    return this.x * this.x + this.y * this.y + this.z * this.z;
   }
-  boolean equal(Point3D q){		// equal
-    return this.x==q.x && this.y==q.y && this.z==q.z;    
+  
+  boolean equal(Point3D q){
+    return this.x == q.x && this.y == q.y && this.z == q.z;    
   }
   public static void main(String[] s){
-    Point3D p=new Point3D();  // creo un punto p
-    p.build(10.0,20.0,30.0);  // ne imposto i valori
-    Point3D q=new Point3D();  // creo un punto q
-    q.build(10.0,20.0,31.0);  // ne imposto i valori
-    System.out.println("Modulo quadro di p: "+p.getModulo2());
-    System.out.println("p e' uguale a q? : "+p.equal(q));
+    Point3D p = new Point3D();  // creo un punto p
+    p.build(10.0, 20.0, 30.0);  // ne imposto i valori
+    Point3D q = new Point3D();  // creo un punto q
+    q.build(10.0, 20.0, 31.0);  // ne imposto i valori
+    System.out.println("Modulo quadro di p: " + p.getModulo2());
+    System.out.println("p e' uguale a q? : " + p.equal(q));
   }
 }
