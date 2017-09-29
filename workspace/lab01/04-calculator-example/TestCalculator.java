b9219a92a8b0f8f86050e21c88a46bb18bc591eb
Danilo Pianini <danilo.pianini@unibo.it>
2017-09-29 12:28:20 +0200

    Fix spacing

--- workspace/lab01/04-calculator-example/TestCalculator.java
+++ workspace/lab01/04-calculator-example/TestCalculator.java
@@ -1,9 +1,9 @@
 class TestCalculator {
   public static void main(String[] args) {
       Calculator calc = new Calculator();
-      System.out.println("1+2=" + calc.add(1, 2));
-      System.out.println("-1-(+2)=" + calc.sub(-1, 2));
-      System.out.println("6*3=" + calc.mul(6, 3));
-      System.out.println("8/4=" + calc.div(8, 4));
+      System.out.println("1 + 2 =" + calc.add(1, 2));
+      System.out.println("-1 - 2 =" + calc.sub(-1, 2));
+      System.out.println("6 * 3 =" + calc.mul(6, 3));
+      System.out.println("8 / 4 =" + calc.div(8, 4));
   }
 }
