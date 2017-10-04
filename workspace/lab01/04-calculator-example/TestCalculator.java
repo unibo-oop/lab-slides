c576717ad3feac8a5f7d8ac958a5fe85dd7028a0
Danilo Pianini <danilo.pianini@unibo.it>
2017-10-04 12:36:06 +0200

    Make TestCalculator compile

--- workspace/lab01/04-calculator-example/TestCalculator.java
+++ workspace/lab01/04-calculator-example/TestCalculator.java
@@ -1,9 +1,14 @@
 class TestCalculator {
   public static void main(String[] args) {
+	  /*
+	   * Uncomment the code below once Calculator has been created!
+	   */
+	  /*
       Calculator calc = new Calculator();
       System.out.println("1 + 2 =" + calc.add(1, 2));
       System.out.println("-1 - 2 =" + calc.sub(-1, 2));
       System.out.println("6 * 3 =" + calc.mul(6, 3));
       System.out.println("8 / 4 =" + calc.div(8, 4));
+      */
   }
 }
