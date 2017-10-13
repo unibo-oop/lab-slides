c44b3684c79a85f7ce16aed51f5e7e7ab69e819a
Danilo Pianini <danilo.pianini@unibo.it>
2017-10-13 15:25:14 +0200

    Improve formatting

--- workspace/lab04/src/it/unibo/oop/lab04/bank2/TestBank2.java
+++ workspace/lab04/src/it/unibo/oop/lab04/bank2/TestBank2.java
@@ -16,49 +16,38 @@ public final class TestBank2 {
     }
 
     public static void main(final String[] args) {
-
         final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
         final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
-
-        // TODO assign actual instances of the realized classes
+        /*
+         *  TODO assign actual instances of the realized classes
+         */
         final BankAccount acc1 = null;
         final BankAccount acc2 = null;
-
         final String balance1 = usr1.getName() + " " + usr1.getSurname() + " account balance";
         final String balance2 = usr2.getName() + " " + usr2.getSurname() + " account balance";
         checkEquality(balance1, 0d, acc1.getBalance());
         checkEquality(balance2, 0d, acc2.getBalance());
-
         final double startAmount = 10000;
         final double withdraw = 15000;
         final double deposit = 10000;
         final double expectedFinal1 = 4995;
         final double expectedFinal2 = 19994.8;
-
         acc1.deposit(usr1.getUserID(), startAmount);
         checkEquality(balance1, startAmount, acc1.getBalance());
-
         acc2.deposit(usr2.getUserID(), startAmount);
         checkEquality(balance2, startAmount, acc2.getBalance());
-
         acc1.withdraw(usr1.getUserID(), withdraw);
         checkEquality(balance1, startAmount - withdraw, acc1.getBalance());
-
         acc2.withdraw(usr2.getUserID(), withdraw);
         checkEquality(balance2, startAmount, acc2.getBalance());
-
         acc1.deposit(usr1.getUserID(), deposit);
         checkEquality(balance1, startAmount - withdraw + deposit, acc1.getBalance());
-
         acc2.deposit(usr2.getUserID(), deposit);
         checkEquality(balance2, startAmount + deposit, acc2.getBalance());
-
         acc1.computeManagementFees(usr1.getUserID());
         checkEquality(balance1, expectedFinal1, acc1.getBalance());
-
         acc2.computeManagementFees(usr2.getUserID());
         checkEquality(balance2, expectedFinal2, acc2.getBalance());
-
     }
 
 }
