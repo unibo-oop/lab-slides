c8309d3ebc03fdbbe9b639a725bf3a6380ce402e
Danilo Pianini <danilo.pianini@unibo.it>
2017-10-30 15:54:33 +0100

    Improve UseSet

--- workspace/lab05-sol/src/it/unibo/oop/lab05/ex1/UseSet.java
+++ workspace/lab05-sol/src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,5 +1,6 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -21,42 +22,50 @@ public final class UseSet {
      * @param s
      *            ignored
      */
-    public static void main(final String[] s) {
+    public static void main(final String[] args) {
         /*
          * Considering the content of "UseCollection, write a program which, in
          * order:
          * 
          * 1) Builds a TreeSet containing Strings
-         * 
-         * 2) Populates such Collection with all the Strings ranging from "1" to
-         * "20"
-         * 
-         * 3) Prints its content
-         * 
-         * 4) Removes all those strings whose represented number is divisible by
-         * three
-         * 
-         * 5) Prints the content of the Set using a for-each costruct
-         * 
-         * 6) Verifies if all the numbers left in the set are even
          */
         final Set<String> set = new TreeSet<>();
+        /*
+         * 2) Populates such Collection with all the Strings ranging from "1" to
+         * "20"
+         */
         for (int i = 1; i <= ELEMS; i++) {
             set.add(Integer.toString(i));
         }
+        /*
+         * 3) Prints its content
+         */
         System.out.println("Initial set: " + set);
-        
-        for (int i = 3; i <= ELEMS; i += 3) {
-            set.remove(Integer.toString(i));
+        /*
+         * 4) Removes all those strings whose represented number is divisible by
+         * three
+         */
+        final Iterator<String> iter = set.iterator();
+        while (iter.hasNext()) {
+            if (Integer.parseInt(iter.next()) % 3 == 0) {
+                iter.remove();
+            }
         }
-        System.out.println("Modified set: " + set);
-
+        /*
+         * 5) Prints the content of the Set using a for-each costruct
+         */
+        for (final String str: set) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+        /*
+         * 6) Verifies if all the numbers left in the set are even
+         */
         final Set<String> set2 = new TreeSet<>();
         for (int i = 2; i <= ELEMS; i += 2) {
             set2.add(Integer.toString(i));
         }
         System.out.println("Even elements: " + set2);
-        
         System.out.println(set2.containsAll(set));
 
     }
