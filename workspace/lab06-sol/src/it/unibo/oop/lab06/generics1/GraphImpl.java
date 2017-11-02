6ffadd5b1de01e262e2ddea56579325df1740439
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-02 17:05:57 +0100

    Fix indentation

--- workspace/lab06-sol/src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ workspace/lab06-sol/src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -11,95 +11,95 @@ import java.util.Set;
 
 public class GraphImpl<N> implements Graph<N> {
 
-	private final Map<N, Set<N>> edges = new HashMap<N, Set<N>>();
+    private final Map<N, Set<N>> edges = new HashMap<N, Set<N>>();
 
-	@Override
-	public void addEdge(final N source, final N target) {
-		if (ensureNodesExist(source, target)) {
-			final Set<N> sourceOutgoingEdges = edges.get(source);
-			if (!sourceOutgoingEdges.contains(target)) {
-				sourceOutgoingEdges.add(target);
-			}
-		}
-	}
+    @Override
+    public void addEdge(final N source, final N target) {
+        if (ensureNodesExist(source, target)) {
+            final Set<N> sourceOutgoingEdges = edges.get(source);
+            if (!sourceOutgoingEdges.contains(target)) {
+                sourceOutgoingEdges.add(target);
+            }
+        }
+    }
 
-	@Override
-	public void addNode(final N node) {
-		edges.putIfAbsent(node, new HashSet<N>());
-	}
+    @Override
+    public void addNode(final N node) {
+        edges.putIfAbsent(node, new HashSet<N>());
+    }
 
-	private boolean ensureNodesExist(final N... nodes) {
-		for (final N node : nodes) {
-			if (!edges.containsKey(node)) {
-				throw new IllegalArgumentException("No such a node: " + node);
-			}
-		}
-		
-		return true;
-	}
+    private boolean ensureNodesExist(final N... nodes) {
+        for (final N node : nodes) {
+            if (!edges.containsKey(node)) {
+                throw new IllegalArgumentException("No such a node: " + node);
+            }
+        }
 
-	private int getNodesCount() {
-		return edges.keySet().size();
-	}
+        return true;
+    }
 
-	@Override
-	public List<N> getPath(final N source, final N target) {
-		if (ensureNodesExist(source, target)) {
-			return graphSearch(source, target, SearchStrategies.BREADTH_FIRST);
-		} else {
-			return Collections.emptyList();
-		}
-		
-	}
+    private int getNodesCount() {
+        return edges.keySet().size();
+    }
 
-	/**
-	 *
-	 * @see http://artint.info/html/ArtInt_51.html
-	 */
-	private List<N> graphSearch(final N source, final N target, final SearchStrategies strategy) {
-		final Deque<Step<N>> fringe = new LinkedList<>();
-		fringe.add(new Step<>(source));
-		final Set<N> alreadyVisited = new HashSet<>();
+    @Override
+    public List<N> getPath(final N source, final N target) {
+        if (ensureNodesExist(source, target)) {
+            return graphSearch(source, target, SearchStrategies.BREADTH_FIRST);
+        } else {
+            return Collections.emptyList();
+        }
 
-		while (!fringe.isEmpty() && alreadyVisited.size() < getNodesCount()) {
-			final Step<N> lastStep = fringe.poll();
-			final N currentNode = lastStep.getPosition();
+    }
 
-			if (currentNode.equals(target)) {
-				return lastStep.getPath();
-			} else if (!alreadyVisited.contains(currentNode)) {
-				alreadyVisited.add(currentNode);
+    /**
+     *
+     * @see http://artint.info/html/ArtInt_51.html
+     */
+    private List<N> graphSearch(final N source, final N target, final SearchStrategies strategy) {
+        final Deque<Step<N>> fringe = new LinkedList<>();
+        fringe.add(new Step<>(source));
+        final Set<N> alreadyVisited = new HashSet<>();
 
-				updateFringe(strategy, fringe, lastStep);
-			}
-		}
+        while (!fringe.isEmpty() && alreadyVisited.size() < getNodesCount()) {
+            final Step<N> lastStep = fringe.poll();
+            final N currentNode = lastStep.getPosition();
 
-		return Collections.emptyList();
-	}
+            if (currentNode.equals(target)) {
+                return lastStep.getPath();
+            } else if (!alreadyVisited.contains(currentNode)) {
+                alreadyVisited.add(currentNode);
 
-	@Override
-	public Set<N> linkedNodes(final N node) {
-		return edges.get(node);
-	}
+                updateFringe(strategy, fringe, lastStep);
+            }
+        }
 
-	@Override
-	public Set<N> nodeSet() {
-		return new HashSet<>(edges.keySet());
-	}
+        return Collections.emptyList();
+    }
 
-	private void updateFringe(final SearchStrategies strategy, final Deque<Step<N>> fringe, final Step<N> lastStep) {
-		final N currentNode = lastStep.getPosition();
-		switch (strategy) {
-		case BREADTH_FIRST:
-			for (final N reachableNode : linkedNodes(currentNode)) {
-				fringe.addLast(new Step<>(lastStep, reachableNode));
-			}
-			break;
-		case DEPTH_FIRST:
-			for (final N reachableNode : linkedNodes(currentNode)) {
-				fringe.addFirst(new Step<>(lastStep, reachableNode));
-			}
-			break;
-		}
-	}
+    @Override
+    public Set<N> linkedNodes(final N node) {
+        return edges.get(node);
+    }
+
+    @Override
+    public Set<N> nodeSet() {
+        return new HashSet<>(edges.keySet());
+    }
+
+    private void updateFringe(final SearchStrategies strategy, final Deque<Step<N>> fringe, final Step<N> lastStep) {
+        final N currentNode = lastStep.getPosition();
+        switch (strategy) {
+        case BREADTH_FIRST:
+            for (final N reachableNode : linkedNodes(currentNode)) {
+                fringe.addLast(new Step<>(lastStep, reachableNode));
+            }
+            break;
+        case DEPTH_FIRST:
+            for (final N reachableNode : linkedNodes(currentNode)) {
+                fringe.addFirst(new Step<>(lastStep, reachableNode));
+            }
+            break;
+        }
+    }
 }
