a0fed5382175b3acbc2bde4b572472ebfaf6e271
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-08 14:53:50 +0100

    Style improvement

--- workspace/lab07-sol/src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
+++ workspace/lab07-sol/src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
@@ -13,15 +13,13 @@ import it.unibo.oop.lab.socialnetwork.User;
  */
 public final class TestSportByStaticInnerClass {
 
-    private TestSportByStaticInnerClass() {
-    }
+    private TestSportByStaticInnerClass() { }
 
     /**
      * @param args
      *            ignored
      */
     public static void main(final String... args) {
-
         /*
          * create 3 sport social network users (SportSocialNetworkUser):
          * 
@@ -37,7 +35,6 @@ public final class TestSportByStaticInnerClass {
                 "Bernie", "Ecclestone", "becclestone", 83);
         final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<>(
                 "Fernando", "Alonso", "falonso", 34);
-
         /*
          * alonso in a fan of soccer and bike and practices F1: add these sports
          * to Alonso
@@ -50,7 +47,6 @@ public final class TestSportByStaticInnerClass {
                 + falonso.hasSport(SportSocialNetworkUserImpl.F1));
         System.out.println("Alonso does not lime volley: "
                 + !falonso.hasSport(SportSocialNetworkUserImpl.VOLLEY));
-
         /*
          * Cassani practiced bike and loves F1 and MotoGP: add these sports to
          * Cassani
@@ -58,23 +54,19 @@ public final class TestSportByStaticInnerClass {
         dcassani.addSport(SportSocialNetworkUserImpl.BIKE);
         dcassani.addSport(SportSocialNetworkUserImpl.F1);
         dcassani.addSport(SportSocialNetworkUserImpl.MOTOGP);
-
         System.out.println("Cassani has been a professional biker: "
                 + dcassani.hasSport(SportSocialNetworkUserImpl.BIKE));
         System.out.println("Cassani does not like soccer: "
                 + !dcassani.hasSport(SportSocialNetworkUserImpl.SOCCER));
-
         /**
          * Ecclestone is the defacto owner of F1 but also is fan of basket: add
-         * these sports to hime
+         * these sports to him
          */
         becclestone.addSport(SportSocialNetworkUserImpl.F1);
         becclestone.addSport(SportSocialNetworkUserImpl.BASKET);
-
         System.out.println("Bernie's the boss when it comes to F1: "
                 + becclestone.hasSport(SportSocialNetworkUserImpl.F1));
         System.out.println("Bernie does love playing also basket: "
                 + becclestone.hasSport(SportSocialNetworkUserImpl.BASKET));
     }
-
 }
