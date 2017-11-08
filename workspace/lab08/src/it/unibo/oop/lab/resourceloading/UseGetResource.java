de8a5e19630895bc34b9ca74a3865c32787f20df
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-08 16:53:48 +0100

    Improve UseGetResource

--- workspace/lab08/src/it/unibo/oop/lab/resourceloading/UseGetResource.java
+++ workspace/lab08/src/it/unibo/oop/lab/resourceloading/UseGetResource.java
@@ -27,21 +27,28 @@ public final class UseGetResource {
      * @throws IOException if an I/O error occurs
      */
     public static void main(final String... args) throws IOException {
-        final URL imgURL = UseGetResource.class.getResource("/images/gandalf.jpg");
-
-        final ImageIcon icon = new ImageIcon(imgURL);
+        /*
+         * Access resources as streams
+         */
         final InputStream in = UseGetResource.class.getResourceAsStream("/settings/settings");
-        final BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        final String line = br.readLine();
-        in.close();
-        final JLabel lab1 = new JLabel(icon);
+        String line = "Error accessing the resource";
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            line = br.readLine();
+        }
         final JLabel lab2 = new JLabel(line);
-
+        /*
+         * Loading of icons and images is made very easy!
+         */
+        final URL imgURL = UseGetResource.class.getResource("/images/gandalf.jpg");
+        final ImageIcon icon = new ImageIcon(imgURL);
+        /*
+         * From now on, it's just plain GUI construction
+         */
+        final JLabel lab1 = new JLabel(icon);
         final JPanel pan = new JPanel();
         pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
         pan.add(lab1);
         pan.add(lab2);
-
         final JFrame f = new JFrame("Image loader");
         f.getContentPane().add(pan);
         f.setResizable(false);
