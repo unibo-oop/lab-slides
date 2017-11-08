02b6d763f857c26714dd507dbb30f6545467a46e
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-08 14:41:13 +0100

    Improve solution of anon1

--- workspace/lab07-sol/src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ workspace/lab07-sol/src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -24,9 +24,7 @@ import it.unibo.oop.lab.socialnetwork.User;
  */
 public final class TestAnonymousComparator {
 
-    private TestAnonymousComparator() {
-
-    }
+    private TestAnonymousComparator() { }
 
     /**
      * Check whether the users in result list are in the same order as those in
@@ -55,18 +53,15 @@ public final class TestAnonymousComparator {
      *            ignored
      */
     public static void main(final String... args) {
-
         /*
          * create 6 social network users
          */
         final SocialNetworkUser<User> kbacon = new SocialNetworkUserImpl<>("Kevin", "Bacon", "kbacon", 56);
-        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington",
-                59);
+        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington", 59);
         final SocialNetworkUser<User> mgladwell = new SocialNetworkUserImpl<>("Malcom", "Gladwell", "mgladwell", 51);
         final SocialNetworkUser<User> ntaleb = new SocialNetworkUserImpl<>("Nicholas", "Taleb", "ntaleb", 54);
         final SocialNetworkUser<User> mrossi = new SocialNetworkUserImpl<>("Mario", "Rossi", "mrossi", 31);
         final SocialNetworkUser<User> pverdi = new SocialNetworkUserImpl<>("Paolo", "Verdi", "pverdi", 24);
-
         // TEST on DENZEL
         dwashington.addFollowedUser("relatives", mrossi);
         dwashington.addFollowedUser("relatives", pverdi);
@@ -74,25 +69,17 @@ public final class TestAnonymousComparator {
         dwashington.addFollowedUser("writers", mgladwell);
         dwashington.addFollowedUser("writers", ntaleb);
         final List<User> denzelUsers = dwashington.getFollowedUsers();
-
         /*
          * Order denzel's followed users incrementally by age:
          * 
          * - define an anonymous comparator to sort incrementally by age
          * 
-         * NOTE: in order to sort a list think about a method of the utility
-         * class java.util.Collections
-         * 
-         * REFER TO LESSON 12-Advanced-Mechanisms.pdf, slide 39
          */
         Collections.sort(denzelUsers, new Comparator<User>() {
             public int compare(final User a, final User b) {
-                final int aAge = a.getAge();
-                final int bAge = b.getAge();
-                return aAge > bAge ? 1 : (aAge == bAge ? 0 : -1);
+                return Integer.compare(a.getAge(), b.getAge());
             }
         });
-
         /*
          * expected Result
          */
@@ -102,12 +89,10 @@ public final class TestAnonymousComparator {
         expectedResult.add(mgladwell);
         expectedResult.add(ntaleb);
         expectedResult.add(kbacon);
-
         System.out.println("[Order by age (increasing) Denzel friends] [TEST] [START]");
         System.out.println("[Order by age (increasing) Denzel friends] [TEST] [RESULT] "
                 + checkUserOrder(expectedResult, denzelUsers));
         System.out.println("[Order by age (increasing) Denzel friends] [TEST] [END]");
-
         // TEST on MARIO ROSSI
         mrossi.addFollowedUser("relatives", pverdi);
         mrossi.addFollowedUser("actors i like", kbacon);
@@ -115,25 +100,16 @@ public final class TestAnonymousComparator {
         mrossi.addFollowedUser("economists", ntaleb);
         mrossi.addFollowedUser("actors i like", dwashington);
         final List<User> rossiUsers = mrossi.getFollowedUsers();
-
         /*
          * Order rossi's followed users by age in decreasing order:
          * 
          * - define an anonymous comparator to sort by age decrementally
-         * 
-         * NOTE: in order to sort a list think about a method of the utility
-         * class Collections
-         * 
-         * REFER TO LESSON 12-Advanced-Mechanisms.pdf, slide 39
          */
         Collections.sort(rossiUsers, new Comparator<User>() {
             public int compare(final User a, final User b) {
-                final int aAge = a.getAge();
-                final int bAge = b.getAge();
-                return aAge < bAge ? 1 : (aAge == bAge ? 0 : -1);
+                return Integer.compare(b.getAge(), a.getAge());
             }
         });
-
         /*
          * expected Result
          */
@@ -143,7 +119,6 @@ public final class TestAnonymousComparator {
         expectedResult.add(ntaleb);
         expectedResult.add(mgladwell);
         expectedResult.add(pverdi);
-
         System.out.println("[Order by age (decreasing) Rossi friends] [TEST] [START]");
         System.out.println("[Order by age (decreasing) Rossi friends] [TEST] [RESULT] "
                 + checkUserOrder(expectedResult, rossiUsers));
