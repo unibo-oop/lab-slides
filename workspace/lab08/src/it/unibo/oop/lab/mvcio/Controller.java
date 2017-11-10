8ad98fda6ac669fe3bcbb370e27d0c6cbd3d9f46
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-10 16:24:32 +0100

    Improve indentation

--- workspace/lab08/src/it/unibo/oop/lab/mvcio/Controller.java
+++ workspace/lab08/src/it/unibo/oop/lab/mvcio/Controller.java
@@ -6,9 +6,8 @@ package it.unibo.oop.lab.mvcio;
 public class Controller {
 
     /*
-     * This class must implement a simple controller responsible of I/O access.
-     * It considers a single file at a time, and it is able to serialize objects
-     * in it.
+     * This class must implement a simple controller responsible of I/O access. It
+     * considers a single file at a time, and it is able to serialize objects in it.
      * 
      * Implement this class in such a way that:
      * 
@@ -16,19 +15,19 @@ public class Controller {
      * 
      * 2) It has a method for getting the current File
      * 
-     * 3) It has a method for getting the path (in form of String) of the
-     * current File
+     * 3) It has a method for getting the path (in form of String) of the current
+     * File
      * 
-     * 4) It has a method that gets a Serializable as input and saves such
-     * Object in the current file. Remember how to use the ObjectOutputStream.
-     * This method may throw IOException.
+     * 4) It has a method that gets a Serializable as input and saves such Object in
+     * the current file. Remember how to use the ObjectOutputStream. This method may
+     * throw IOException.
      * 
-     * 5) By default, the current file is "output.dat" inside the user home
-     * folder. A String representing the local user home folder can be accessed
-     * using System.getProperty("user.home"). The separator symbol (/ on *nix, \
-     * on Windows) can be obtained as String through the method
-     * System.getProperty("file.separator"). The combined use of those methods
-     * leads to a software that run correctly on every platform.
+     * 5) By default, the current file is "output.dat" inside the user home folder.
+     * A String representing the local user home folder can be accessed using
+     * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
+     * Windows) can be obtained as String through the method
+     * System.getProperty("file.separator"). The combined use of those methods leads
+     * to a software that run correctly on every platform.
      */
 
 }
