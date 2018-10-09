b11a2f07d0535142e3d1326c8438ba92d8c5c38c
Danilo Pianini <danilo.pianini@unibo.it>
2018-10-09 18:14:02 +0200

    Improve lab03 exercise for next year

--- workspace/lab03/src/it/unibo/oop/lab03/interfaces/TestBankAccount.java
+++ workspace/lab03/src/it/unibo/oop/lab03/interfaces/TestBankAccount.java
@@ -2,24 +2,50 @@ package it.unibo.oop.lab03.interfaces;
 
 public final class TestBankAccount {
 
-    private TestBankAccount() { }
+    private TestBankAccount() {
+    }
 
     public static void main(final String[] args) {
         /*
-         * 1) Creare l' AccountHolder relativo a Mario Rossi con id 1 2) Creare
-         * l' AccountHolder relativo a Luigi Bianchi con id 2 3) Dichiarare due
-         * variabili (acc1 e acc2) di tipo BankAccount 4) Creare in acc1 un
-         * nuovo oggetto di tipo SimpleBankAccount relativo al conto di Mario Rossi
-         * (ammontare iniziale = 0) 5) Creare in acc2 un nuovo oggetto di tipo
-         * StrictBankAccount relativo al conto di Luigi Bianchi (ammontare
-         * iniziale = 0) 6) Prima riflessione: perchè è stato possibile fare la
-         * new di due classi diverse in variabili dello stesso tipo? 7)
-         * Depositare 10000$ in entrambi i conti 8) Prelevare 15000$ in entrambi
-         * i conti 9) Stampare in stdout l'ammontare corrente 10) Qual'è il
-         * risultato e perchè? 11) Depositare nuovamente 10000$ in entrambi i
-         * conti 11) Invocare il metodo computeManagementFees su entrambi i
-         * conti 12) Stampare a video l'ammontare corrente 13) Qual'è il
-         * risultato e perchè?
+         * 1) Creare l' AccountHolder relativo a Mario Rossi con id 1
          */
+        /*
+         * 2) Creare l' AccountHolder relativo a Luigi Bianchi con id 2
+         */
+        /*
+         * 3) Dichiarare due variabili di tipo BankAccount ed inizializzarle,
+         * rispettivamente, con oggetti di tipo SimpleBankAccount per il conto di Mario
+         * Rossi (ammontare iniziale = 0), e di tipo StrictBankAccount per il conto di
+         * Luigi Bianchi (ammontare iniziale = 0)
+         */
+        /*
+         * 4) Prima riflessione: perché è stato possibile fare la new di due classi
+         * diverse in variabili dello stesso tipo?
+         */
+        /*
+         * 5) Depositare €10000 in entrambi i conti
+         */
+        /*
+         * 6) Prelevare €15000$ da entrambi i conti
+         */
+        /*
+         * 7) Stampare in stdout l'ammontare corrente
+         */
+        /*
+         * 8) Qual è il risultato e perché?
+         */
+        /*
+         * 9) Depositare nuovamente €10000 in entrambi i conti
+         */
+        /*
+         * 10) Invocare il metodo computeManagementFees su entrambi i conti
+         */
+        /*
+         * 11) Stampare a video l'ammontare corrente
+         */
+        /*
+         * 12) Qual è il risultato e perché?
+         */
+
     }
 }
