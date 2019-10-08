98fd3ad73c16160395d0a13af91bc7c2ac01be83
Angelo Croatti <a.croatti@unibo.it>
2019-10-08 15:13:18 +0200

    lab03 fix

--- workspace/lab03/src/oop/lab03/bank/SimpleBankAccount.java
+++ workspace/lab03/src/oop/lab03/bank/SimpleBankAccount.java
@@ -5,7 +5,7 @@ public class SimpleBankAccount {
 	private int userID;
     /*
      * Aggiungere i seguenti campi:
-     * - double balace: ammontare del conto
+     * - double balance: ammontare del conto
      * - int userID: id del possessore del conto (già dichiarato: si veda al riguardo il metodo checkUser dichiarato in fondo alla classe)
      * - int nTransactions: numero delle operazioni effettuate
      * - static double ATM_TRANSACTION_FEE = 1: costo delle operazioni via ATM
