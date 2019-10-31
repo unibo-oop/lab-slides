dc79303226d9bb33949feb0887a035472a123d0e
Danilo Pianini <danilo.pianini@unibo.it>
2019-10-31 11:57:05 +0100

    Simplify serialVersionUID

--- workspace/lab06-sol/src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ workspace/lab06-sol/src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -8,6 +8,6 @@ public class WrongAccountHolderException extends IllegalArgumentException {
     /**
      * 
      */
-    private static final long serialVersionUID = -6628031695600635427L;
+    private static final long serialVersionUID = 1L;
 
 }
