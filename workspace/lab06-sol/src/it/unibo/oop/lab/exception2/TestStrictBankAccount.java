9cb52e34aa435a5603156ab4e6fa90f9f461467d
Danilo Pianini <danilo.pianini@unibo.it>
2019-10-31 11:47:08 +0100

    Improve comment

--- workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -20,14 +20,20 @@ public class TestStrictBankAccount {
      */
     @Test
     public void testBankOperations() {
-        // 1) Creare due StrictBankAccount assegnati a due AccountHolder a
-        // scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
+        /*
+         * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
+         * scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
+         */
         final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
         final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
         final StrictBankAccount account1 = new StrictBankAccount(usr1.getUserID(), INITIAL_AMOUNT, 10);
         final StrictBankAccount account2 = new StrictBankAccount(usr2.getUserID(), INITIAL_AMOUNT, 10);
-        // 2) Effetture un numero di operazioni a piacere per testare il lancio
-        // delle eccezioni per i casi di interesse
+        /*
+         * 2) Effetture un numero di operazioni a piacere per verificare che le
+         * eccezioni create vengano lanciate soltanto quando opportuno, cioè in presenza
+         * di un id utente errato, oppure al superamento del numero di operazioni ATM
+         * gratuite.
+         */
         try {
             account1.deposit(4, 100);
             fail();
