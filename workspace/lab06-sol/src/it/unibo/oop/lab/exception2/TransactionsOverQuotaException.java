8ce4de1a1151b0e0b99b970c7b7b8e39cdb34a21
Danilo Pianini <danilo.pianini@unibo.it>
2019-10-31 11:47:57 +0100

    Simplify serialVersionUID

--- workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
+++ workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
@@ -8,6 +8,6 @@ public class TransactionsOverQuotaException extends IllegalStateException {
     /**
      * 
      */
-    private static final long serialVersionUID = 5903967606158446940L;
+    private static final long serialVersionUID = 1L;
 
 }
