cda8b8141f520e9062f078c197626635a449530d
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-05 18:02:53 +0100

    Improve vertical spacing

--- workspace/lab07/src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
+++ workspace/lab07/src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
@@ -13,15 +13,13 @@ import it.unibo.oop.lab.socialnetwork.User;
  */
 public final class TestSportByStaticInnerClass {
 
-    private TestSportByStaticInnerClass() {
-    }
+    private TestSportByStaticInnerClass() { }
 
     /**
      * @param args
      *            ignored
      */
     public static void main(final String... args) {
-
         /*
          * create 3 sport social network users (SportSocialNetworkUser):
          * 
@@ -34,7 +32,6 @@ public final class TestSportByStaticInnerClass {
         final SportSocialNetworkUserImpl<User> dcassani = null;
         final SportSocialNetworkUserImpl<User> becclestone = null;
         final SportSocialNetworkUserImpl<User> falonso = null;
-
         // TODO
         /*
          * alonso is a fan of soccer and bike and practices F1: add these sports
@@ -46,24 +43,20 @@ public final class TestSportByStaticInnerClass {
          */
         System.out.println("Alonso practices F1: " + falonso.hasSport(SportSocialNetworkUserImpl.F1));
         System.out.println("Alonso does not like volley: " + !falonso.hasSport(SportSocialNetworkUserImpl.VOLLEY));
-
         /*
          * Cassani practiced bike and loves F1 and MotoGP
          */
         dcassani.addSport(SportSocialNetworkUserImpl.BIKE);
         dcassani.addSport(SportSocialNetworkUserImpl.F1);
         dcassani.addSport(SportSocialNetworkUserImpl.MOTOGP);
-
         System.out.println("Cassani has been a professional biker: "
                 + dcassani.hasSport(SportSocialNetworkUserImpl.BIKE));
         System.out.println("Cassani does not like soccer: " + !dcassani.hasSport(SportSocialNetworkUserImpl.SOCCER));
-
         /**
          * Ecclestone is the de-facto owner of F1 but also a fan of basket.
          */
         becclestone.addSport(SportSocialNetworkUserImpl.F1);
         becclestone.addSport(SportSocialNetworkUserImpl.BASKET);
-
         System.out.println("Bernie's the boss when it comes to F1: "
                 + becclestone.hasSport(SportSocialNetworkUserImpl.F1));
         System.out.println("Bernie does love playing also basket: "
