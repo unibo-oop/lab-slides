98032f6696c6aad8fe3f7c2b136012a7b9f9ae8a
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-06 12:05:34 +0100

    Comment exit and use resource loading

--- workspace/lab08-sol/src/it/unibo/oop/lab/advanced/DrawNumberApp.java
+++ workspace/lab08-sol/src/it/unibo/oop/lab/advanced/DrawNumberApp.java
@@ -1,9 +1,9 @@
 package it.unibo.oop.lab.advanced;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,8 +30,8 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
             view.start();
         }
         final Configuration.Builder configurationBuilder = new Configuration.Builder();
-        try {
-            for (final String configLine: Files.readAllLines(Paths.get(configFile))) {
+        try (var contents = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(configFile)))) {
+            for (var configLine = contents.readLine(); configLine != null; configLine = contents.readLine()) {
                 final String[] lineElements = configLine.split(":");
                 if (lineElements.length == 2) {
                     final int value = Integer.parseInt(lineElements[1].trim());
@@ -88,6 +88,12 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
 
     @Override
     public void quit() {
+        /*
+         * A bit harsh. A good application should configure the graphics to exit by
+         * natural termination when closing is hit. To do things more cleanly, attention
+         * should be paid to alive threads, as the application would continue to persist
+         * until the last thread terminates.
+         */
         System.exit(0);
     }
 
