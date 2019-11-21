5f96d31ad3252f28ef0a01779292b0fbfeaea1be
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-21 12:50:41 +0100

    Remove unused inport

--- workspace/lab09-sol/src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrixWithFutures.java
+++ workspace/lab09-sol/src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrixWithFutures.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
