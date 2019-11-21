e59efff9f346f4655d48e928aeb20f7e43903e0e
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-21 13:05:01 +0100

    Leverage parallel stream machinery

--- workspace/lab09-sol/src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrixWithStreams.java
+++ workspace/lab09-sol/src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrixWithStreams.java
@@ -1,6 +1,5 @@
 package it.unibo.oop.lab.workers02;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
@@ -26,45 +25,6 @@ public class MultiThreadedSumMatrixWithStreams implements SumMatrix {
         this.nthread = nthread;
     }
 
-    private class Worker extends Thread {
-
-        private final double[][] matrix;
-        private final int startpos;
-        private final int nelem;
-        private double res;
-
-        /**
-         * Builds a new worker.
-         * 
-         * @param matrix
-         *            the matrix to be summed
-         * @param startpos
-         *            the start position for the sum in charge to this worker
-         * @param nelem
-         *            the no. of element for him to sum
-         */
-        Worker(final double[][] matrix, final int startpos, final int nelem) {
-            super();
-            this.matrix = Arrays.copyOf(matrix, matrix.length);
-            this.startpos = startpos;
-            this.nelem = nelem;
-        }
-
-        @Override
-        public void run() {
-            for (int i = startpos; i < matrix.length && i < startpos + nelem; i++) {
-                for (final double d : this.matrix[i]) {
-                    this.res += d;
-                }
-            }
-        }
-
-        public double getResult() {
-            return this.res;
-        }
-
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -76,10 +36,20 @@ public class MultiThreadedSumMatrixWithStreams implements SumMatrix {
          */
         return IntStream.iterate(0, start -> start + size)
                 .limit(nthread)
+                /*
+                 * We do not create thread ourselves. We decide how tasks should be done, and we
+                 * ask the Stream library to spawn the required threads.
+                 */
                 .parallel()
-                .mapToObj(start -> new Worker(matrix, start, size))
-                .peek(Runnable::run)
-                .mapToDouble(Worker::getResult)
+                .mapToDouble(start -> {
+                    double result = 0;
+                    for (int i = start; i < matrix.length && i < start + size; i++) {
+                        for (final double d : matrix[i]) {
+                            result += d;
+                        }
+                    }
+                    return result;
+                })
                 .sum();
     }
 }
