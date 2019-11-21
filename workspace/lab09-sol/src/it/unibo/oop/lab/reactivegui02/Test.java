82e908eace4de1f4a120f3f9e1cc628ef1308567
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-21 11:15:36 +0100

    Improve consistency

--- workspace/lab09-sol/src/it/unibo/oop/lab/reactivegui02/Test.java
+++ workspace/lab09-sol/src/it/unibo/oop/lab/reactivegui02/Test.java
@@ -1,32 +1,30 @@
 package it.unibo.oop.lab.reactivegui02;
 
 /**
- * 
- * TestMatrix for second reactive GUI.
- *
+ * Exercise on a reactive GUI.
  */
 public final class Test {
 
     private Test() {
-
     }
 
     /*
-     * Realizzare una classe C2GUI con costruttore a zero argomenti, tale che
-     * quando istanziata crei un JFrame con l'aspetto mostrato nella figura
-     * allegata (e contatore inizialmente posto a zero).
+     * Realizzare una classe ConcurrentGUI con costruttore privo di argomenti,
+     * tale che quando istanziata crei un JFrame con l'aspetto mostrato nella
+     * figura allegata (e contatore inizialmente posto a zero).
      * 
      * Il contatore venga aggiornato incrementandolo ogni 100 millisecondi
      * circa, e il suo nuovo valore venga mostrato ogni volta (l'interfaccia sia
-     * quindi reattiva). Alla pressione del pulsante "down", il conteggio venga
-     * da lì in poi aggiornato decrementandolo; alla pressione del pulsante
-     * "up", il conteggio venga da lì in poi aggiornato incrementandolo; e così
-     * via, in modo alternato.
+     * quindi reattiva).
+     * 
+     * Alla pressione del pulsante "down", il conteggio venga da lì in poi
+     * aggiornato decrementandolo; alla pressione del pulsante "up", il
+     * conteggio venga da lì in poi aggiornato incrementandolo; e così via, in
+     * modo alternato.
      * 
      * Alla pressione del pulsante "stop", il conteggio si blocchi, e i tre
-     * pulsanti vengano disabilitati.
-     * 
-     * Per far partire l'applicazioni si tolga il commento nel main qui sotto.
+     * pulsanti vengano disabilitati. Per far partire l'applicazioni si tolga il
+     * commento nel main qui sotto.
      * 
      * Suggerimenti: - si mantenga la struttura dell'esercizio precedente - per
      * pilotare la direzione su/giù si aggiunga un flag booleano all'agente --
@@ -37,11 +35,10 @@ public final class Test {
     /**
      * 
      * @param args
-     *            unused
+     *            possible args to pass (not used)
      * 
      */
     public static void main(final String... args) {
         new ConcurrentGUI();
-
     }
 }
