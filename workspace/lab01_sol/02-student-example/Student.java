3e50495bd7ad4b64972e7420c619d6d25df03b9e
Placu <andrea.placuzzi@studio.unibo.it>
2020-09-22 15:28:33 +0200

    place student id after the surname as in Readme requests

--- workspace/lab01_sol/02-student-example/Student.java
+++ workspace/lab01_sol/02-student-example/Student.java
@@ -1,20 +1,20 @@
 class Student {
+    int id;
     String name;
     String surname;
-    int id;
     int matriculationYear;
 
-    void build(int id, String name, String surname, int matriculationYear) {
-        this.id = id;
+    void build(String name, String surname, int id, int matriculationYear) {
         this.name = name;
         this.surname = surname;
+        this.id = id;
         this.matriculationYear = matriculationYear;
     }
 
     void printStudentInfo() {
-        System.out.println("Student id: " + id);
         System.out.println("Student name: " + name);
         System.out.println("Student surname: " + surname);
+        System.out.println("Student id: " + id);
         System.out.println("Student matriculationYear: " + matriculationYear);
     }
 }
--- workspace/lab01_sol/02-student-example/TestStudent.java
+++ workspace/lab01_sol/02-student-example/TestStudent.java
@@ -1,19 +1,19 @@
 class TestStudent {
   public static void main(String[] args) {
       Student mario = new Student();
-      mario.build(1014, "Mario", "Rossi", 2019);
+      mario.build("Mario", "Rossi", 1014, 2019);
       mario.printStudentInfo();
 
       Student luigi = new Student();
-      luigi.build(1015, "Luigi", "Gentile", 2019);
+      luigi.build("Luigi", "Gentile", 1015, 2019);
       luigi.printStudentInfo();
 
       Student simone = new Student();
-      simone.build(1016, "Simone", "Bianchi", 2018);
+      simone.build("Simone", "Bianchi", 1016, 2018);
       simone.printStudentInfo();
 
       Student andrea = new Student();
-      andrea.build(1017, "Andrea", "Bracci", 2017);
+      andrea.build("Andrea", "Bracci", 1017, 2017);
       andrea.printStudentInfo();
   }
 }
