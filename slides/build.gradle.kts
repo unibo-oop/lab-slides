45ccc18ca817a231cfd5a6ee74f8288cb072bc62
Danilo Pianini <danilo.pianini@gmail.com>
2020-09-24 10:57:22 +0200

    Add automated build

---
+++ slides/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id ("org.danilopianini.gradle-latex") version "0.2.6"
+}
+
+latex {
+    (projectDir.listFiles() ?: emptyArray())
+        .asSequence()
+        .filter { it.isDirectory }
+        .map { folder ->
+            folder?.listFiles { file: File ->
+                file.extension == "tex" && file.name.take(2) == folder.name.take(2)
+            }
+                    ?.firstOrNull()
+        }
+        .filterNotNull()
+        .map { it.relativeTo(projectDir) }
+        .sortedBy { it.name }
+        .forEach {
+            it.path()
+        }
+}
