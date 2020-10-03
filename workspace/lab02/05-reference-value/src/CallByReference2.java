4de43c8f07fc0b409432dac7d5d09816f6e6fe24
Roberto Casadei <robyonrails@gmail.com>
2020-10-03 11:51:16 +0200

    Adjust names in lab 2 ex 5

--- workspace/lab02/05-reference-value/src/CallByReference2.java
+++ workspace/lab02/05-reference-value/src/CallByReference2.java
@@ -1,16 +1,16 @@
 class CallByReference2 {
 
-    void increment(Point3D point) {
+    void print(Point3D point) {
         point = new Point3D();
         point.build(11, 12, 13);
-        System.out.println("[increment] x=" + point.x + ";y=" + point.y + ";z=" + point.z);
+        System.out.println("[print] x=" + point.x + ";y=" + point.y + ";z=" + point.z);
     }
 
     public static void main(final String[] args) {
         Point3D p1 = new Point3D();
         p1.build(1, 2, 3);
         CallByReference2 cbr2 = new CallByReference2();
-        cbr2.increment(p1);
+        cbr2.print(p1);
         System.out.println("[main] x=" + p1.x + ";y=" + p1.y + ";z=" + p1.z);
     }
 }
