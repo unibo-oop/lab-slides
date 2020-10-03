81b41a99d6ffe4da563d90c868fd4f5c80898aa2
Roberto Casadei <robyonrails@gmail.com>
2020-10-03 11:35:10 +0200

    Adjustments to lab 2 ex 4 readme in source file (on arguments and shell/escape/...) @NiccoMlt

--- workspace/lab02/04-arguments/src/oop/lab02/cli/TestCommandLine.java
+++ workspace/lab02/04-arguments/src/oop/lab02/cli/TestCommandLine.java
@@ -7,22 +7,29 @@ public class TestCommandLine {
          * Write the main method in such a way that it iterates through the
          * array of arguments, printing each one along with the length of the
          * string - the String class provides a
-         * 
+         *
          * int length()
-         * 
+         *
          * method that returns the length of the String.
-         * 
+         *
          * Example output:
-
-
-           java oop.lab02.cli.TestCommandLine it's a trap
-
-           The provided arguments are:
-            * it's, 4 characters long
-            * a, 1 characters long
-            * trap, 4 characters long
-
-
+         *
+         * $ java oop.lab02.cli.TestCommandLine it's a trap
+         *
+         * The provided arguments are:
+         *  * it's, 4 characters long
+         *  * a, 1 characters long
+         *  * trap, 4 characters long
+         *
+         * NB: depending on the shell in use, the quote symbol may open a string
+         * and create issues with the interpretation of the above command.
+         * In that case, close the string or use Ctrl+C to interrupt
+         * the current command. If you do want to provide an argument "it's",
+         * you might be able to solve the issue in the following two ways:
+         *   1) By using quotes to wrap the argument
+         *   $ java oop.lab02.cli.TestCommandLine "it's" a trap
+         *   2) By escaping the single quote
+         *   $ java oop.lab02.cli.TestCommandLine it\'s a trap
          */
     }
 
