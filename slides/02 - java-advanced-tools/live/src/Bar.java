0e369977037883368a8fe53da54c22b91eaf7862
Roberto Casadei <robyonrails@gmail.com>
2021-10-02 16:09:44 +0200

    Updates to lab 02

---
+++ slides/02 - java-advanced-tools/live/src/Bar.java
@@ -0,0 +1,7 @@
+package foo.bar;
+
+public class Bar {
+    public static void main(String[] args){
+        System.out.println("Bar");
+    }
+}
---
+++ slides/02 - java-advanced-tools/live/src/Foo.java
@@ -0,0 +1,7 @@
+package foo.bar;
+
+public class Foo {
+    public static void main(String[] args){
+        System.out.println("FooBar");
+    }
+}
