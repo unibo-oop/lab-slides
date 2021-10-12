8eddfd1294173389bd73742063a34d50300bcb93
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-12 12:16:19 +0200

    Add staging/delta images (downloaded on the fly)

---
+++ slides/04 - dvcs-1/download-resources.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+download_image() {
+    DESTINATION="img/${1##*/}"
+    echo $1
+    echo $DESTINATION
+    if [ ! -f "$DESTINATION" ]; then
+        wget -O "$DESTINATION" "$1"
+    fi
+}
+
+download_image "https://git-scm.com/book/en/v2/images/deltas.png"
+download_image "https://git-scm.com/book/en/v2/images/snapshots.png"
